0b42e58c32514e34793fbf7d7720255c56438304
Lim Ah Seng <lim.ahseng@dhs.sg>
2013-04-23 12:03:47 +0800

    first commit

---
+++ bankaccount.py
@@ -0,0 +1,87 @@
+# bankaccount.py
+
+class Account():
+    """ bank account super class """
+
+    def __init__(self, account_no, balance):
+        """ constructor method """
+        self.__account_no = account_no
+        self.__balance = balance
+
+    def get_account_no(self):
+        """ accessor method to retrieve account number """
+        return self.__account_no
+
+    def get_balance(self):
+        """ accessor method to retrieve balance """
+        return self.__balance
+
+##    def set_balance(self, new_balance):
+##        """ modifier/mutator method to update balance """
+##        self.__balance = new_balance
+
+    def deposit(self, amount):
+        """ modifier/mutator method to increase balance """
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        """ modifier/mutator method to decrease balance """
+        self.__balance -= amount
+
+    def display(self):
+        """ helper/support method to show account info """
+        print("Account No:", self.__account_no)
+        print("Balance:", self.__balance)
+
+
+class SavingsAccount(Account):
+    """ savings account subclass """
+
+    def __init__(self, account_no, balance, interest):
+        """ subclass constructor method """
+        super().__init__(account_no, balance)
+        self.__interest = interest
+
+    def calc_interest(self):
+        """ helper/support method to compute interest """
+        self.deposit(self.get_balance() * (1 + self.__interest))
+        # self.__balance # illegal
+
+    def display(self):
+        """ helper/support method to show savings account info """
+        super().display()
+        print("Savings interest:", self.__interest)
+
+
+class CurrentAccount(Account):
+    """ current account subclass """
+
+    def __init__(self, account_no, balance, overdraft):
+        """ subclass constructor method """
+        super().__init__(account_no, balance)
+        self.__overdraft = overdraft
+
+    def withdraw(self, amount): # overrides superclass withdraw
+        """ helper/support method to withdraw up to overdraft limit """
+        if amount > (self.get_balance() + self.__overdraft): # cannot withdraw more than overdraft limit
+            print("Withdrawal amount exceeds overdraft limit.")
+        else:
+            super().withdraw(amount)
+        
+
+    def display(self):
+        """ helper/support method to show current account info """
+        super().display()
+        print("Overdraft limit:", self.__overdraft)
+        
+
+# main
+savings_acct1 = SavingsAccount("C01", 0, 0.01)
+savings_acct1.deposit(500)
+savings_acct1.calc_interest()
+savings_acct1.display()
+
+current_acct1 = CurrentAccount("C01", 0, 500)
+current_acct1.withdraw(300)
+current_acct1.withdraw(300)
+current_acct1.display()
---
+++ person.py
@@ -0,0 +1,78 @@
+# person.py
+
+class Person():
+    """ person super class """
+    def __init__(self, pid, name):
+        """ constructor for person """
+        self.__pid = pid
+        self.__name = name
+
+    def get_pid(self):
+        """ accessor for person id """
+        return self.__pid
+
+    def get_name(self):
+        """ accessor for name """
+        return self.__name
+
+    def display(self):
+        """ support/helper to show person info """
+        print("ID:", self.__pid)
+        print("Name:", self.__name)
+
+
+class Student(Person):
+    """ student subclass """
+    def __init__(self, pid, name, classid):
+        """ constructor for student """
+        super().__init__(pid, name)
+        self.__classid = classid
+
+    def get_classid(self):
+        """ accessor for student class """
+        return self.__classid
+
+    def set_classid(self, new_classid):
+        """ modifier/mutator for student class """
+        self.__classid = new_classid
+
+    def display(self): # overriding
+        """ support/helper to show student info """
+        super().display()
+        print("Class:", self.__classid)
+
+
+class Staff(Person):
+    """ staff subclass """
+    
+    def __init__(self, pid, name, department):
+        """ constructor for staff """
+        super().__init__(pid, name)
+        self.__department = department
+
+    def get_department(self):
+        """ accessor for staff department """
+        return self.__department
+
+    def set_department(self, new_department):
+        """ modifier/mutator for staff department """
+        self.__department = new_department
+
+    def display(self): # overriding
+        """ support/helper to show staff info """
+        super().display()
+        print("Department:", self.__department)
+
+
+# main
+# instantiate student and staff objects
+student1 = Student("S01", "Lim Ah Seng", "13Y5C23")
+staff1 = Staff("A05", "Robert Yeo", "Computing")
+
+person_list = []
+person_list.append(student1)
+person_list.append(staff1)
+for person in person_list:
+    person.display() # polymorphism
+
+
